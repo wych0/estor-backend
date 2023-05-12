6cc11e7192a35e538e3bb67162ada4299338173d
wych0 <wycho01@wp.pl>
2023-05-12 22:55:37 +0200

    Started working on order router

---
+++ routes/order.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const Order = require('../models/order')
+
+router.post('/create',
+
+async (req, res) => {
+    
+}
+)
+
+module.exports = router
