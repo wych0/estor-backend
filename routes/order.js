03b599e96a831e99ee2fe09f7b9581c9f12882b4
wych0 <wycho01@wp.pl>
2023-05-14 23:42:39 +0200

    Making validation for placing order

--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,10 @@
 const {validationResult, check} = require('express-validator')
+const {ObjectId} = require('mongodb')
 const express = require('express')
 const router = express.Router()
 const Order = require('../models/order')
 const User = require('../models/User')
+const Product = require('../models/product')
 
 router.post('/create',
 check('userEmail')
@@ -19,7 +21,32 @@ check('userEmail')
 check('cost')
 .escape()
 .trim()
-.notEmpty().withMessage('Cost field cannot be empty'),
+.notEmpty().withMessage('Cost field cannot be empty')
+.isDecimal().withMessage('Cost field must be a number'),
+
+check('products')
+.escape()
+.trim()
+.custom(async (products) =>{
+    if(products.length===0){
+        return Promise.reject('Products array cannot be empty')
+    }
+    await Promise.all(products.map(async (productID) => {
+        const product = await Product.findOne({_id: new ObjectId(productID)})
+        if(!product){
+            return Promise.reject('Product with at least one of these id not found')
+        }
+        if(product.isSold){
+            return Promise.reject('At least one of the products has been sold')
+        }
+    }))
+    
+    for(let i=0; i<products.length; i++){
+        if(products.indexOf(products[i]) !== i){
+            return Promise.reject('There is a duplicate product')
+        }
+    }
+}),
 
 async (req, res) => {
     const errors = validationResult(req)
@@ -32,6 +59,10 @@ async (req, res) => {
         cost,
         products
     })
+    products.forEach(async(productID) => {
+        await Product.findOneAndUpdate({_id: new ObjectId(productID)}, {isSold: true})
+    })
+    
     await order.save()
     res.status(200).json({message: "Placed order"})
 }
