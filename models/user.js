d083e25bb16d73f0ad85863f82ad1c679e90c6cc
wych0 <wycho01@wp.pl>
2023-05-16 17:59:42 +0200

    Added status of order in order schema

--- models/user.js
+++ models/user.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema({
     role:{
         type: String,
         default: 'klient' 
+    },
+    accountStatus:{
+        type: String,
+        default: 'Aktywne'
     }
 })
 
