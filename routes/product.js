7f3fac3609a60c836e16d78bf205a3e24d936e05
wych0 <wycho01@wp.pl>
2023-05-17 00:24:32 +0200

    Deleting product from database

--- routes/product.js
+++ routes/product.js
@@ -50,4 +50,38 @@ async (req, res) => {
 }
 )
 
+router.delete('/',
+check('productID')
+.trim()
+.escape()
+.notEmpty().withMessage('ProductID field cannot be empty')
+.custom(async(productID)=>{
+    const product = await Product.findById(new ObjectId(productID))
+    if(!product){
+        return Promise.reject('Product with that id not found')
+    }
+    if(product.isSold===true){
+        return Promise.reject('You cant delete sold product from database')
+    }
+}),
+async (req, res) => { 
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    const userID = req.cookies.userID
+    if(!userID){
+        return res.status(403).json({message: 'Login to delete product'})
+    }
+    const user = await User.findById(new ObjectId(userID))
+    if(user.role!=='admin'){
+        return res.status(403).json({message: 'You cannot delete product from database'})
+    }
+    const {productID} = req.body
+    const result = await Product.deleteOne({_id: new ObjectId(productID)})
+    console.log(result)
+    res.status(200).json({message: "Deleted product"})
+}
+)
+
 module.exports = router
