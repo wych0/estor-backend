3a26eab6f773b74674e5e327d38fc3c89179bff0
wych0 <wycho01@wp.pl>
2023-05-16 17:58:18 +0200

    Only admin can add new product to database

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const {ObjectId} = require('mongodb')
 const {validationResult, check} = require('express-validator')
 const router = express.Router()
 const Product = require('../models/product')
+const User = require('../models/User')
+const cookieParser = require('cookie-parser')
+router.use(cookieParser())
 
 router.post('/create',
 check(['name', 'brand'])
@@ -21,11 +25,18 @@ check('image')
 .notEmpty().withMessage('Image field cannot be empty')
 .matches(/[^\\s]+(.*?)\.(jpg|jpeg|png|)$/i).withMessage('Wrong file name, remember about extension'),
 
-
 async (req, res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
+    }
+    const userID = req.cookies.userID
+    if(!userID){
+        return res.status(403).json({message: 'You have to be logged in'})
+    }
+    user = await User.findById(new ObjectId(userID))
+    if(!user || user.role!=='admin'){
+        return res.status(403).json({message: 'You cant add product to database'})
     }
     const {name, brand, price, image} = req.body
     const product = new Product({
