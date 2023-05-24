c4c7b521553d6d13835563c91f43f6a5bd181df5
Albert Wychowaniec <wycho01@wp.pl>
2023-05-24 10:31:57 +0200

    Updating user cart items whenever one of product included is sold

--- routes/cart.js
+++ routes/cart.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {ObjectId} = require('mongodb')
 const router = express.Router()
 const User = require('../models/User')
+const Product = require('../models/product')
 const cookieParser = require('cookie-parser')
 router.use(cookieParser())
 
@@ -15,6 +16,15 @@ async(req, res)=>{
     if(!user){
         return res.status(404).json({message: 'User not found'})
     }
+    const updatedCartItems = []
+    for(const item of user.cartItems){
+        const product = await Product.findById(item._id)
+        if(!product.isSold){
+            updatedCartItems.push(product)
+        }
+    }
+    user.cartItems = updatedCartItems
+    await user.save()
     return res.status(200).json({cartItems: user.cartItems})
 })
 
