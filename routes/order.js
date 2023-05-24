4f177cdd676da37a2bac54333293463787cfe86a
Albert Wychowaniec <wycho01@wp.pl>
2023-05-24 10:33:17 +0200

    Editing router to create order

--- routes/order.js
+++ routes/order.js
@@ -8,49 +8,13 @@ const Product = require('../models/product')
 const cookieParser = require('cookie-parser')
 router.use(cookieParser())
 
-router.post('/create',
-check('userEmail')
-.escape()
-.trim()
-.notEmpty().withMessage('UserEmail field cannot be empty')
-.custom(async (userEmail) => {
-    const user = await User.findOne({email: userEmail})
-    if(!user){
-        return Promise.reject('User with that e-mail not found')
-    }
-}),
+router.post('/',
 
 check('cost')
 .escape()
 .trim()
 .notEmpty().withMessage('Cost field cannot be empty')
 .isDecimal().withMessage('Cost field must be a number'),
-
-check('products')
-.escape()
-.trim()
-.custom(async (products) =>{
-    if(!Array.isArray(products)){
-        return Promise.reject('Products field must be an array')
-    }
-    if(products.length===0){
-        return Promise.reject('Products array cannot be empty')
-    }
-    await Promise.all(products.map(async (productID) => {
-        const product = await Product.findOne({_id: new ObjectId(productID)})
-        if(!product){
-            return Promise.reject('Product with at least one of these id not found')
-        }
-        if(product.isSold){
-            return Promise.reject('At least one of the products has been sold')
-        }
-    }))
-    for(let i=0; i<products.length; i++){
-        if(products.indexOf(products[i]) !== i){
-            return Promise.reject('There is a duplicate product')
-        }
-    }
-}),
 
 check(['address.name','address.country','address.secName','address.street','address.city','address.postalCode','address.email'])
 .trim()
@@ -65,15 +29,19 @@ async (req, res) => {
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
     }
-    const {cost, userEmail, products, address} = req.body
+    const {cost, userEmail, address} = req.body
     const userID = req.cookies.userID
     if(!userID){
         return res.status(403).json({message: 'Login to place order'})
     }
+    const user = await User.findById(new ObjectId(userID))
+    if(user.cartItems.length===0){
+        return res.status(403).json({message: 'Your cart is empty'})
+    }
     const order = new Order({
         userEmail,
         cost,
-        products,
+        products: user.cartItems,
         userID,
         address:{
             name: address.name,
@@ -85,10 +53,14 @@ async (req, res) => {
             country: address.country
         }
     })
-    products.forEach(async(productID) => {
-        await Product.findByIdAndUpdate(new ObjectId(productID), {isSold: true})
-    })
-    
+
+    for (const item of user.cartItems){
+        const product = await Product.findByIdAndUpdate(new ObjectId(item._id), {isSold:true})
+        await product.save()
+    }
+
+    user.cartItems = []
+    await user.save()
     await order.save()
     res.status(200).json({message: 'Placed order'})
 }
